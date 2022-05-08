b424d525be4c3108321c132c86b04ff0ea6ad67a
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-05-08 19:04:43 +0530

    Add project files.

---
+++ OverloadingProgram/Auth.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverloadingProgram
+{
+    public class Authentication
+    {
+        public bool LoginUser(string email, string password)
+        {
+            if (email == "ayaj123@gmail.com" && password == "12345")
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool LoginUser(string mobile)
+        {
+            if (!string.IsNullOrEmpty(mobile))
+            {
+                //send otp
+                return true ;
+
+            }
+            return false ;
+        }
+    }
+}
---
+++ OverloadingProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace OverloadingProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Authentication auth=new Authentication();
+
+            string mobile = "9340336471";
+            string email = "ayaj123@gmail.com";
+            string password = "12345";
+
+            Console.WriteLine( auth.LoginUser(email,password));
+
+        }
+    }
+}
